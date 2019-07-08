b30cdd86ad1b3ebd34524abd0c134018ad222655
Michael Passow <passowmichael@yahoo.com>
2019-07-08 16:03:03 -0400

    added timer

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -9,24 +9,72 @@ function check() {
     var question5 = document.quiz.question5.value;
 
     var correct = 0;
+    var incorrect = 0;
 
+    //QUESTION 1
     if(question1 === "CSS") {
         correct++;
     }
+    else if (question1 === 'Ruby' || 'HTML' || 'C++'){
+        incorrect++
+    }
+    //QUESTION 2
     if(question2 === "True") {
         correct++;
     }
+    else if (question2 === 'False'){
+        incorrect++
+    }
+    //QUESTION3
     if(question3 === "False") {
         correct++;
     }
+    else if (question3 === 'True'){
+        incorrect++
+    }
+    //QUESTION4
     if(question4 === "Sheet") {
         correct++;
     }
+    else if (question4 === 'Show' || 'Storage' || 'Store'){
+        incorrect++
+    }
+    //QUESTION5
     if(question5 === "All of the above") {
         correct++;
     }
-
+    else if (question5 === 'CSS' || 'HTML' || 'Javscript'){
+        incorrect++
+    }
     document.getElementById("after_submit").style.visibility = "visible";
     document.getElementById("numberCorrect").innerHTML = "You got " + correct + " correct!";
+    document.getElementById("numberIncorrect").innerHTML = "You got " + incorrect + " incorrect!";
 
 }
+
+
+function startTimer(duration, display) {
+    var timer = //duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer <= 0) {
+            timer = duration;
+        }
+    }, 1000);
+
+   
+}
+
+jQuery(function ($) {
+    var fiveMinutes = 60 * 5,
+        display = $('#timer');
+    startTimer(fiveMinutes, display);
+});
+
